4a0ca06cd6ceb1133267a10c90d6a6352ce0aa29
Yuriy Kuptsevych <y.kuptsevich@rj-games.com>
2023-06-08 10:41:57 +0300

    presented as package

---
+++ Runtime/Entities/ViewModelValue.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+
+namespace UISystem.Runtime.Entities
+{
+	public class ViewModelValue<T> : IViewModelItem
+	{
+		private T _value;
+
+		private Action<T> _onChanged;
+
+		public T Value
+		{
+			get => _value;
+			set
+			{
+				if (!EqualityComparer<T>.Default.Equals(_value, value))
+				{
+					_value = value;
+					_onChanged?.Invoke(value);
+				}
+			}
+		}
+
+		public ViewModelValue(T init)
+		{
+			_value = init;
+		}
+
+		public void Connect(Action<T> onChanged)
+		{
+			var connected = _onChanged != null;
+			if (connected)
+			{
+				throw new Exception("Already connected");
+			}
+
+			_onChanged = onChanged;
+			if (typeof(T).IsValueType || !EqualityComparer<T>.Default.Equals(_value, default))
+			{
+				_onChanged.Invoke(Value);
+			}
+		}
+
+		public void Reset()
+		{
+			_onChanged = default;
+			_value = default;
+		}
+	}
+}
